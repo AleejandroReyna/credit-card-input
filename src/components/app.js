dfd761857bc03b78b4e83c8b4c75813232944852
Alejandro Reyna <aleejandroreyna@gmail.com>
2016-02-08 12:03:38 -0600

    Credit Card replace whitespaces

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react"
+import CardCredit from "./credit-card-input"
+
+let types = {
+  'Visa': /^4/,
+  'MasterCard': /^5[1-5]/,
+  'American Express': /^3[47]/
+};
 
 export default class App extends Component {
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <CardCredit types={types}/>
+    )
   }
 }
---
+++ src/components/credit-card-input.js
@@ -0,0 +1,56 @@
+import React, {Component} from "React"
+
+export default class CardCredit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.checkType = this.checkType.bind(this);
+  }
+
+  process(number) {
+    let type = this.checkType(number);
+    let text = this.insertSpaces(number);
+    text.trim();
+    return {
+      text: text,
+      type: type
+    };
+  }
+
+  filterWhitespace(text) {
+    return text.replace(/\s/g, '');
+  }
+
+  insertSpaces(text) {
+    return text.replace(/(.{4})/g, '$1 ');
+  }
+
+  checkType(text) {
+    let types = this.props.types;
+    for (var type in types) {
+      if (text.match(types[type])) {
+        return type;
+      }
+    }
+    return '';
+  }
+
+  handleChange(e) {
+    let newValue = e.target.value;
+    let newNumber = this.filterWhitespace(newValue);
+    this.setState({number: newNumber});
+  }
+
+  render() {
+    let { text: text, type: type } = this.process(this.state.number);
+    return(
+      <div>
+        <input type="text" value={text} onChange={this.handleChange} />
+        <input type="text" value={type} readOnly />
+      </div>
+    )
+  }
+}
